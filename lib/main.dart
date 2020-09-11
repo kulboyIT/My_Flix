77ee8b64b157db45d773902809ba1b33ea9d2e24
Aditya Pal <adityapalmzn12@gmaiil.com>
2020-09-11 12:33:46 +0530

    Fix Some bugs

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,10 @@ void main(){
  ));
 
 }
+// echo "# My_Flix" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M master
+// git remote add origin https://github.com/Adityapalmzn12/My_Flix.git
+// git push -u origin master
